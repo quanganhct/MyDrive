39101fa62a6d6b16d247964fbe55c28f02f8e954
trakers33 <guillaume.rebmann33@gmail.com>
2015-01-08 00:38:46 +0700

    Webservlet hide Html in the system

---
+++ src/main/java/net/mydrive/ws/RestCallServlet.java
@@ -0,0 +1,20 @@
+ 
+/*
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.mydrive.ws;
+
+/**
+ *
+ * @author guillaumerebmann
+ */
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class RestCallServlet extends HttpServlet {
+    
+}
--- src/main/java/net/mydrive/ws/WebServlet.java
+++ src/main/java/net/mydrive/ws/WebServlet.java
@@ -1,5 +1,25 @@
-package net.mydrive.ws; 
+ 
+/*
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class WebServlet {
-	
+package net.mydrive.ws;
+
+/**
+ *
+ * @author guillaumerebmann
+ */
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class WebServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+        System.out.println(request);
+        response.sendRedirect("index.html");
+    }
 }
