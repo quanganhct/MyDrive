1cff1de6287b166a3e2ab4a9a78dd8d754086827
NGUYEN Quang Anh <nguyenquanganh@TheAncient.local>
2015-03-04 10:35:52 +0700

    test

--- src/main/java/net/mydrive/ws/WebServlet.java
+++ src/main/java/net/mydrive/ws/WebServlet.java
@@ -1,4 +1,3 @@
- 
 /*
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,42 +16,51 @@ import javax.servlet.http.*;
 
 public class WebServlet extends HttpServlet {
 
-    /**
-     *
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    @Override
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws ServletException, IOException {
-        //System.out.println("1 "+request.getRequestURI());
-    	
-    	//handleCallbackIfRequired(request, response);
-    	
-    	//loginIfRequired(request, response);
-    	
-        System.out.println("2"+request.getServletPath()+"2");
-        
-        
-        if(request.getServletPath().equals("/index")){
-            if(request.getSession().getAttribute("user_id") == null){
-                //response.sendRedirect("/login");
-                request.getRequestDispatcher("login.html").forward(request, response);
-            }else{
-                //response.sendRedirect("/index.html");
-                request.getRequestDispatcher("index.html").forward(request, response);
-            }
-           
-        }else if(request.getServletPath().equals("/login")){
-           //response.sendRedirect("/login.html");
-            request.getRequestDispatcher("login.html").forward(request, response);
-        }else{
-            System.out.println("Error 404");
-            //response.sendRedirect("/login.html");
-            request.getRequestDispatcher("login.html").forward(request, response);
-        }
-    }
+	/**
+	 *
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// System.out.println("1 "+request.getRequestURI());
+
+		// handleCallbackIfRequired(request, response);
+
+		// loginIfRequired(request, response);
+
+		// System.out.println("2" + request.getServletPath() + "2");
+
+		if (request.getSession().getAttribute("user_id") == null
+				&& !request.getServletPath().equals("/login")) {
+			System.out.println(request.getServletPath());
+			response.sendRedirect("http://test.com:5000/login");
+		} else {
+
+			if (request.getServletPath().equals("/index")) {
+				if (request.getSession().getAttribute("user_id") == null) {
+					// response.sendRedirect("/login");
+					request.getRequestDispatcher("login.html").forward(request,
+							response);
+				} else {
+					// response.sendRedirect("/index.html");
+					request.getRequestDispatcher("index.html").forward(request,
+							response);
+				}
+
+			} else if (request.getServletPath().equals("/login")) {
+				// response.sendRedirect("/login.html");
+				request.getRequestDispatcher("login.html").forward(request,
+						response);
+			} else {
+				System.out.println("Error 404");
+				// response.sendRedirect("/login.html");
+				request.getRequestDispatcher("login.html").forward(request,
+						response);
+			}
+		}
+	}
 }
